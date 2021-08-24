eeed00e60725e51236322f5f8b32b8fa49c89b7d
hanskw4267 <hanskurniaw@gmail.com>
2021-08-24 17:59:37 +0800

    updated script again

--- Shell/linux_devel_setup.bash
+++ Shell/linux_devel_setup.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
 # Post install script to setup a linux development environment
 
@@ -10,10 +10,9 @@ if [ $(whoami) == "root" ]; then     #"guarding against root execution"
 fi
 
 setup () {
-  echo "Do you wish to install $1??"
-  select reply in "Yes" "No"; do
-    case $reply in
-      Yes )
+  read -r -p "Do you wish to install $1 [y/N]??" reply
+    case "$reply" in
+      [yY][eE][sS]|[yY] )
         sudo apt install -y $1
         if [ $? -eq 0 ]; then
           echo "<--------------------------- $1 Install OK"
@@ -21,13 +20,12 @@ setup () {
           echo "<--------------------------- $1 Install failed"
           exit $?
         fi
-        break;;
+        ;;
       * )
         echo "Okay, no problem. :) Let's move on!"
-        echo "Skipping $1" 
-        break;;
+        echo "Skipping $1"
+        ;;
     esac
-  done
 }
 
 echo "-------------------------------------------------"
@@ -48,57 +46,60 @@ setup "wget"
 setup "curl"
 setup "openssh-server"
 setup "build-essential cmake"
-setup "virtualenv python3-pip pipenv"
+setup "virtualenv python3-pip"
 
 echo "-------------------------------------------------"
 echo "<-- Git install and config -->"
-echo "Do you wish to install git??"
-select reply in "Yes" "No"; do
-  case $reply in
-    Yes )
-      echo "Installing Git"
-      sudo apt install -y git
-      echo "What username do you want to use for git?"
-      read git_username
-      git config --global user.name "$git_username"
-      echo "-----------------------------------------"
-      echo "What user email do you want to use for git?"
-      read git_useremail
-      git config --global user.email "$git_useremail"
-      break;;
+read -r -p "Do you wish to install git [y/N]??" reply
+  case "$reply" in
+      [yY][eE][sS]|[yY] )
+        echo "Installing Git"
+        sudo apt install -y git
+        read -r -p "Do you wish to configure git [y/N]??" reply
+            case "$reply" in
+                [yY][eE][sS]|[yY] )
+                    echo "What username do you want to use for git?"
+                    read git_username
+                    git config --global user.name "$git_username"
+                    echo "-----------------------------------------"
+                    echo "What user email do you want to use for git?"
+                    read git_useremail
+                    git config --global user.email "$git_useremail"
+                    ;;
+                * )
+                    echo "Okay, no problem. :) Let's move on!"
+                    echo "Skipping Git config"
+                    ;;
+        ;;
     * ) 
-      echo "Okay, no problem. :) Let's move on!"
-      echo "Skipping Git"
-      break;;
+        echo "Okay, no problem. :) Let's move on!"
+        echo "Skipping Git"
+        ;;
   esac
-done
 
 echo "-------------------------------------------------"
 echo "<-- VSCode install and config -->"
-echo "Do you wish to install VSCode??"
-select reply in "Yes" "No"; do
-  case $reply in
-    Yes )
-      echo "Installing VSCode"
-      sudo apt install -y apt-transport-https
-      wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-      sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-      sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-      sudo apt update
-      sudo apt install -y code
-      break;;
+read -r -p "Do you wish to install VSCode [y/N]??" reply
+  case "$reply" in
+    [yY][eE][sS]|[yY] )
+        echo "Installing VSCode"
+        sudo apt install -y apt-transport-https
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+        sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+        sudo apt update
+        sudo apt install -y code
+        ;;
     * )
-      echo "Okay, no problem. :) Let's move on!"
-      echo "Skipping VSCode"
-      break;;
+        echo "Okay, no problem. :) Let's move on!"
+        echo "Skipping VSCode"
+        ;;
   esac
-done
 
 setup_extension () {
-  echo "Do you wish to install extension: $1??"
-  select reply in "Yes" "No"; do
-    case $reply in
-      Yes )
+  read -r -p "Do you wish to install $1 [y/N]??" reply
+    case "$reply" in
+      [yY][eE][sS]|[yY] )
         code --install-extension $1
         if [ $? -eq 0 ]; then
           echo "<--------------------------- $1 Install OK"
@@ -106,53 +107,48 @@ setup_extension () {
           echo "<--------------------------- $1 Install failed"
           exit $?
         fi
-        break;;
+        ;;
       * )
         echo "Okay, no problem. :) Let's move on!"
         echo "Skipping $1"
-        break;;
+        ;;
     esac
-  done
 }
 
 echo "-------------------------------------------------"
-echo "Do you wish to install some of my favourite VSCode extentions? (you can choose for each)"
-select reply in "Yes" "No"; do
-  case $reply in
-    Yes )
-      echo "Installing VSCode extensions"
-      setup_extension eamodio.gitlens
-      setup_extension coenraads.bracket-pair-colorizer-2
-      setup_extension ms-vscode.cpptools-extension-pack 
-      setup_extension mhutchie.git-graph
-      setup_extension wayou.vscode-todo-highlight
-      setup_extension ms-python.python
-      setup_extension oderwat.indent-rainbow
-      setup_extension pkief.material-icon-theme
-      setup_extension esbenp.prettier-vscode
-      break;;
+read -r -p "Do you wish to install some of my favourite VSCode extentions? (you can choose for each) [y/N]??" reply
+  case "$reply" in
+      [yY][eE][sS]|[yY] )
+        echo "Installing VSCode extensions"
+        setup_extension eamodio.gitlens
+        setup_extension coenraads.bracket-pair-colorizer-2
+        setup_extension ms-vscode.cpptools-extension-pack 
+        setup_extension mhutchie.git-graph
+        setup_extension wayou.vscode-todo-highlight
+        setup_extension ms-python.python
+        setup_extension oderwat.indent-rainbow
+        setup_extension pkief.material-icon-theme
+        setup_extension esbenp.prettier-vscode
+        ;;
     * )
-      echo "Okay, no problem. :) Let's move on!"
-      echo "Skipping VSCode extensions"
-      break;;
+        echo "Okay, no problem. :) Let's move on!"
+        echo "Skipping VSCode extensions"
+        ;;
   esac
-done
 
 echo "-------------------------------------------------"
 echo "Do you wish to install spotify?"
-echo "Make sure you HAVE snap store installed!"
-select reply in "Yes" "No"; do
-  case $reply in
-    Yes )
-      echo "Installing spotify"
-      snap install spotify
-      break;;
-    * )
-      echo "Okay, no problem. :) Let's move on!"
-      echo "Skipping spotify"
-      break;;
+read -r -p "Make SURE you have snap store installed [y/N]??" reply
+  case "$reply" in
+    [yY][eE][sS]|[yY] )
+    echo "Installing spotify"
+    snap install spotify
+    ;;
+  * )
+    echo "Okay, no problem. :) Let's move on!"
+    echo "Skipping spotify"
+    ;;
   esac
-done
 
 echo "<-- cleaning -->"
 sudo apt autoremove -y
